df88de0ba201f6b0e38cb29f100dccbebf3c14d5
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-09 09:29:12 +0700

    Add project files.

---
+++ SK-Lib-cstddef-offsetof-Bagian4__CPP/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstddef>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+struct S {
+    char   m0;
+    double m1;
+    short  m2;
+    char   m3;
+    //  private: int z; // warning: 'S' is a non-standard-layout type
+};
+int main() {
+    std::cout
+        << "offset of char   m0 = " << offsetof(S, m0) << '\n'
+        << "offset of double m1 = " << offsetof(S, m1) << '\n'
+        << "offset of short  m2 = " << offsetof(S, m2) << '\n'
+        << "offset of char   m3 = " << offsetof(S, m3) << '\n';
+
+    _getch();
+    return 0;
+}
